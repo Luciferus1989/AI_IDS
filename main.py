7e23321129cd2eb0c03d8051b885250f4887ce87
Mr Anton <anton.velitsky@gmail.com>
2023-12-06 20:55:31 +1200

    Test

--- main.py
+++ main.py
@@ -1,84 +1,66 @@
 import os
-from openread import open_csv_file
+from openread import open_csv_file, combine_normal, combine_attack
+import pandas as pd
 
-current_directory = os.path.dirname(os.path.abspath(__file__))
+if os.path.isfile('normal.cvs'):
+    print('Fail {} is founded in dir.'.format('normal.cvs'))
+    df_normal = pd.read_csv('normal.cvs.csv')
+else:
+    df_normal = combine_normal()
+    df_normal.to_csv('normal.cvs')
 
-
-file_1 = 'Dataset_Iot/Normal/win7_normal_2.csv'
-backdoor = [('Dataset_Iot/Backdoor/win7_backdoor_normal_1.csv', 'backdoor'),
-            ('Dataset_Iot/Backdoor/win7_backdoor_normal_2.csv', 'backdoor')]
-ddos = [('Dataset_Iot/DDos/win7_DDoS_normal_1.csv', 'DDoS'),
-        ('Dataset_Iot/DDos/win10_DDoS_normal_1.csv', 'DDoS')]
-dos = [('Dataset_Iot/Dos/win7_DoS_normal_1.csv', 'dos'),
-       ('Dataset_Iot/Dos/win10_DoS_normal_1.csv', 'dos')]
-infection = [('Dataset_Iot/Infection/win7_injection_normal_1.csv', 'infection'),
-             ('Dataset_Iot/Infection/win10_injection_normal_1.csv', 'infection')]
-mitm = [('Dataset_Iot/MITM/win7_MIMT_normal_1.csv', 'mitm'),
-        ('Dataset_Iot/MITM/win10_MITM_normal_1.csv', 'mitm')]
-password = [('Dataset_Iot/Password/win7_password_normal_1.csv', 'password'),
-            ('Dataset_Iot/Password/win10_password_normal_1.csv', 'password')]
-runsomware = [('Dataset_Iot/Runsomware/win7_runsomware_normal_1.csv', 'runsomware'),
-              ('Dataset_Iot/Runsomware/win7_runsomware_normal_2.csv', 'runsomware'),
-              ('Dataset_Iot/Runsomware/win7_runsomware_normal_3.csv', 'runsomware')]
-scanning = [('Dataset_Iot/Scanning/win7_scanning_normal_1.csv', 'scanning'),
-            ('Dataset_Iot/Scanning/win10_scanning_normal_1.csv', 'scanning'),
-            ('Dataset_Iot/Scanning/win10_scanning_normal_2.csv', 'scanning'),
-            ('Dataset_Iot/Scanning/win10_scanning_normal_3.csv', 'scanning')]
-xss = [('Dataset_Iot/XSS/win7_XSS_normal_1.csv', 'xss'),
-       ('Dataset_Iot/XSS/win10_XSS_normal_1.csv', 'xss')]
-normal = [('Dataset_Iot/Normal/win7_normal_2.csv', 'normal'),
-          ('Dataset_Iot/Normal/win7_normal_3.csv', 'normal'),
-          ('Dataset_Iot/Normal/win10_normal_1.csv', 'normal'),
-          ('Dataset_Iot/Normal/win10_normal_2.csv', 'normal'),
-          ('Dataset_Iot/Normal/win10_normal_3.csv', 'normal'),
-          ('Dataset_Iot/Normal/win10_normal_4.csv', 'normal')]
-
-a = backdoor[0][0]
-
-file_path = os.path.join(current_directory, a)
-
-df = open_csv_file(file_path)
+if os.path.isfile('attack.cvs'):
+    print('Fail {} is founded in dir.'.format('attack.cvs'))
+    df_attack = pd.read_csv('attack.cvs')
+else:
+    df_attack = combine_attack()
+    df_attack.to_csv('attack.cvs')
 
 
-X, y = load_and_preprocess_data()
 
-# Encode categorical labels (if applicable)
-label_encoder = LabelEncoder()
-y = label_encoder.fit_transform(y)
 
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Standardize features
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
 
-# Reshape the data for CNN input (assuming 1D features)
-X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], 1)
-X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], 1)
 
-# Convert labels to one-hot encoding
-y_train_one_hot = tf.keras.utils.to_categorical(y_train, num_classes=num_classes)
-y_test_one_hot = tf.keras.utils.to_categorical(y_test, num_classes=num_classes)
-
-# Define the CNN model
-model = models.Sequential()
-model.add(layers.Conv1D(32, kernel_size=3, activation='relu', input_shape=(X_train.shape[1], 1)))
-model.add(layers.MaxPooling1D(pool_size=2))
-model.add(layers.Flatten())
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(num_classes, activation='softmax'))
-
-# Compile the model
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-
-# Train the model
-model.fit(X_train, y_train_one_hot, epochs=10, batch_size=64, validation_data=(X_test, y_test_one_hot))
-
-# Evaluate the model on the test set
-loss, accuracy = model.evaluate(X_test, y_test_one_hot)
-print(f'Test Loss: {loss}, Test Accuracy: {accuracy}')
-
-# Make predictions
-predictions = model.predict(X_test)
+# X, y = load_and_preprocess_data()
+#
+# # Encode categorical labels (if applicable)
+# label_encoder = LabelEncoder()
+# y = label_encoder.fit_transform(y)
+#
+# # Split the data into training and testing sets
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+#
+# # Standardize features
+# scaler = StandardScaler()
+# X_train = scaler.fit_transform(X_train)
+# X_test = scaler.transform(X_test)
+#
+# # Reshape the data for CNN input (assuming 1D features)
+# X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], 1)
+# X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], 1)
+#
+# # Convert labels to one-hot encoding
+# y_train_one_hot = tf.keras.utils.to_categorical(y_train, num_classes=num_classes)
+# y_test_one_hot = tf.keras.utils.to_categorical(y_test, num_classes=num_classes)
+#
+# # Define the CNN model
+# model = models.Sequential()
+# model.add(layers.Conv1D(32, kernel_size=3, activation='relu', input_shape=(X_train.shape[1], 1)))
+# model.add(layers.MaxPooling1D(pool_size=2))
+# model.add(layers.Flatten())
+# model.add(layers.Dense(64, activation='relu'))
+# model.add(layers.Dense(num_classes, activation='softmax'))
+#
+# # Compile the model
+# model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+#
+# # Train the model
+# model.fit(X_train, y_train_one_hot, epochs=10, batch_size=64, validation_data=(X_test, y_test_one_hot))
+#
+# # Evaluate the model on the test set
+# loss, accuracy = model.evaluate(X_test, y_test_one_hot)
+# print(f'Test Loss: {loss}, Test Accuracy: {accuracy}')
+#
+# # Make predictions
+# predictions = model.predict(X_test)
--- openread.py
+++ openread.py
@@ -1,5 +1,6 @@
 import json
 import pandas as pd
+import os
 import csv
 
 # column_names = [
@@ -46,7 +47,6 @@ import csv
 
 
 def open_csv_file(file_name):
-    file_1 = 'Dataset_Iot/Normal/win7_normal_2.csv'
     with open(file_name, 'r') as file:
         reader = csv.reader(file, delimiter=',')
         header = next(reader)
@@ -57,3 +57,47 @@ def open_csv_file(file_name):
                      '|' + column.split('\\')[-1].replace('% ', '') for column in data.columns[1:]])
 
     return data
+
+
+def combine_normal():
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    normal = [('Dataset_Iot/Normal/win7_normal_2.csv', 'normal'),
+              ('Dataset_Iot/Normal/win7_normal_3.csv', 'normal'),
+              ('Dataset_Iot/Normal/win10_normal_1.csv', 'normal'),
+              ('Dataset_Iot/Normal/win10_normal_2.csv', 'normal'),
+              ('Dataset_Iot/Normal/win10_normal_3.csv', 'normal'),
+              ('Dataset_Iot/Normal/win10_normal_4.csv', 'normal')]
+    df = [open_csv_file(os.path.join(current_directory, file)) for file, _ in normal]
+
+    return pd.concat(df, ignore_index=True)
+
+
+def combine_attack():
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    backdoor = [('Dataset_Iot/Backdoor/win7_backdoor_normal_1.csv', 'backdoor'),
+                ('Dataset_Iot/Backdoor/win7_backdoor_normal_2.csv', 'backdoor')]
+    ddos = [('Dataset_Iot/DDos/win7_DDoS_normal_1.csv', 'DDoS'),
+            ('Dataset_Iot/DDos/win10_DDoS_normal_1.csv', 'DDoS')]
+    dos = [('Dataset_Iot/Dos/win7_DoS_normal_1.csv', 'dos'),
+           ('Dataset_Iot/Dos/win10_DoS_normal_1.csv', 'dos')]
+    infection = [('Dataset_Iot/Infection/win7_injection_normal_1.csv', 'infection'),
+                 ('Dataset_Iot/Infection/win10_injection_normal_1.csv', 'infection')]
+    mitm = [('Dataset_Iot/MITM/win7_MIMT_normal_1.csv', 'mitm'),
+            ('Dataset_Iot/MITM/win10_MITM_normal_1.csv', 'mitm')]
+    password = [('Dataset_Iot/Password/win7_password_normal_1.csv', 'password'),
+                ('Dataset_Iot/Password/win10_password_normal_1.csv', 'password')]
+    runsomware = [('Dataset_Iot/Runsomware/win7_runsomware_normal_1.csv', 'runsomware'),
+                  ('Dataset_Iot/Runsomware/win7_runsomware_normal_2.csv', 'runsomware'),
+                  ('Dataset_Iot/Runsomware/win7_runsomware_normal_3.csv', 'runsomware')]
+    scanning = [('Dataset_Iot/Scanning/win7_scanning_normal_1.csv', 'scanning'),
+                ('Dataset_Iot/Scanning/win10_scanning_normal_1.csv', 'scanning'),
+                ('Dataset_Iot/Scanning/win10_scanning_normal_2.csv', 'scanning'),
+                ('Dataset_Iot/Scanning/win10_scanning_normal_3.csv', 'scanning')]
+    xss = [('Dataset_Iot/XSS/win7_XSS_normal_1.csv', 'xss'),
+           ('Dataset_Iot/XSS/win10_XSS_normal_1.csv', 'xss')]
+    # for base in [backdoor, ddos, dos, infection, mitm, password, runsomware, scanning, xss]:
+    df = [open_csv_file(os.path.join(current_directory, file))
+          for base in [backdoor, ddos, dos, infection, mitm, password, runsomware, scanning, xss] for file, _ in base]
+
+    return pd.concat(df, ignore_index=True)
+
